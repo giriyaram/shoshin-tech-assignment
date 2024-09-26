d9d86641be51c03d72a78bd0a0cce1df33cbb7a3
Giri Yaram <yaramgiri@gmail.com>
2024-09-26 21:06:37 +0530

    added sample data

---
+++ src/lib/data.js
@@ -0,0 +1,43 @@
+export const announcementData = [
+    {
+        title: "Outing schedule for every departement",
+        time: "5 minutes ago"
+    },
+    {
+        title: "Meeting HR Department",
+        time: "Yesterday, 12:30 PM"
+    },
+    {
+        title: "IT Department need two more talents for UX/UI Designer position",
+        time: "Yesterday, 10:20 PM"
+    },
+];
+
+export const scheduleData = [
+    {
+        status: "Priority",
+        items:[
+            {
+                title: "Review candidate applications",
+                time: "Today - 11.30 AM"
+            },
+        ],
+    },
+    {
+        status: "Other",
+        items:[
+            {
+                title: "Interview with candidates",
+                time: "Today - 10.30 AM"
+            },
+            {
+                title: "Short meeting with product designer from IT Departement",
+                time: "Today - 09.30 AM"
+            },
+            {
+                title: "Interview with candidates",
+                time: "Today - 10.30 AM"
+            },
+        ],
+    }
+];
